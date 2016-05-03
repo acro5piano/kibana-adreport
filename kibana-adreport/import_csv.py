af154aaee35ae8d53fd9ea35589ba7fdb4472b98
gosho-kazuya <gosho-kazuya@919.jp>
2016-05-03 13:18:08 +0900

    Add index mapping

--- kibana-adreport/import_csv.py
+++ kibana-adreport/import_csv.py
@@ -6,21 +6,43 @@ Import csv data and create index to elasticsearch.
 import pandas as pd
 from elasticsearch import Elasticsearch
 
+INDEX_NAME = 'kango-oshigoto'
+CSV_FILE_PATH = 'data/kango-oshigoto.csv'
+
 # Initialize Elasticsearch
 es = Elasticsearch('localhost:9200')
 try:
-	es.indices.delete('ad_reports')
+	es.indices.delete(INDEX_NAME)
 except:
 	pass
 
 # Open csv file
-df = pd.read_csv('sample_data.csv')
+df = pd.read_csv(CSV_FILE_PATH)
 fields = df.columns
+
+# Mapping
+
+
+mappings = {
+    "template": "*",
+    "mappings": {
+        "_default_": {
+            "properties" : {
+                "URL" : { "type" : "string", "index" : "not_analyzed" },
+                "hello_work" : { "type" : "boolean"},
+                "pref" : { "type" : "string", "index" : "not_analyzed" },
+            }
+        }
+    }
+}
+
+# Create index
+es.indices.create(index=INDEX_NAME, body=mappings)
 
 # Import data
 for _, row in df.iterrows():
 	data_dict = {}
 	for f in fields:
 		data_dict[f] = row[f]
-	es.index(index='ad_reports', doc_type='test', body=data_dict) 
+	es.index(index=INDEX_NAME, doc_type='test', body=data_dict)
 
